6846e073154d82daad707c359503385be888fd0f
pierre21200 <pierrepotin21@gmail.com>
2020-07-10 12:13:05 +0200

    essaie get API

--- home.js
+++ home.js
@@ -1,22 +1,31 @@
-
 fetch('http://localhost:3000/api/teddies')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+// # 
+
+// const url = 'http://localhost:3000/api/teddies'
+// const getTeddies = async (url) => {
+//   const response = await fetch(url);
+//   return await response.json();
+// }
 
 
 
-  const card1 = document.getElementById('teddy1');
+function renderProduct (productName, productId, productImg, productPrice) {
+  const products = document.querySelector('#product-item');
+  const section = document.createElement('section');
+  products.appendChild(section);
+  section.innerHTML = '<img alt="${productName}" src="${productImg}">' 
+  '<h6 class="product-title">${productName}</h6>'
+  '<p class="price">${productPrice / 100} € </p>'
+  '<button class="site-btn btn-line" type="button"><a href="product.html?id=${productId}">Sélectionner</a> </button>'
+}
 
-  const teddy1 = document.getElementById('5be9c8541c9d440000665243');
-  teddy1.innerHTML = data;
-
-
-
-  var myImage = document.querySelector('.my-image');
-  fetch('flowers.jpg').then(function(response) {
-    return response.blob();
-  }).then(function(blob) {
-    var objectURL = URL.createObjectURL(blob);
-    myImage.src = objectURL;
+const displayProducts = async () => {
+  const products = await getTeddies(url)
+  products.forEach(product => {
+    renderProduct(productName, product._id, product.imageUrl, product.price);
   });
+}
+
 
