7adb2a4ee04c4f327978aa99eecad0a848c2ea17
pierre21200 <pierrepotin21@gmail.com>
2020-07-13 16:41:38 +0200

    pageProduct

--- home.js
+++ home.js
@@ -1,31 +1,87 @@
-fetch('http://localhost:3000/api/teddies')
-  .then(response => response.json())
-  .then(data => console.log(data))
-// # 
+// desole les commentaires sont sans accents, c'est la galere avec mon clavier
 
-// const url = 'http://localhost:3000/api/teddies'
-// const getTeddies = async (url) => {
-//   const response = await fetch(url);
-//   return await response.json();
-// }
+// la function retourne juste la promesse renvoye par le fetch
+const getTeddies = url => {
+  return fetch(url).then(response => response.json());
+};
 
+// l'url appelle dans la function getTeddies
+const url = "http://localhost:3000/api/teddies";
 
+// on selectionne d'abord l'element du dom ou l'on va ajoute tous nos elements
+const products = document.querySelector("#product-item");
 
-function renderProduct (productName, productId, productImg, productPrice) {
-  const products = document.querySelector('#product-item');
-  const section = document.createElement('section');
-  products.appendChild(section);
-  section.innerHTML = '<img alt="${productName}" src="${productImg}">' 
-  '<h6 class="product-title">${productName}</h6>'
-  '<p class="price">${productPrice / 100} € </p>'
-  '<button class="site-btn btn-line" type="button"><a href="product.html?id=${productId}">Sélectionner</a> </button>'
-}
+// function permettant de cree nos produits et les ajouter a l'element principale du dom qui est en l'ocurence products (ligne 10)
+// pour exectuer la function il faut l'appeler, voir plus bas
+const renderProducts = () => {
+  // appelle de la function getTeddies afin de l'executer
+  getTeddies(url).then(teddies => {     // teddies = response ? 
+    // ouvre ta console
+    console.log("liste des teddies: ", teddies); // teddies = response ? 
 
-const displayProducts = async () => {
-  const products = await getTeddies(url)
-  products.forEach(product => {
-    renderProduct(productName, product._id, product.imageUrl, product.price);
+    // on itere sur le tableau de teddies renvoye par le backend
+    teddies.forEach((teddy, index) => { // teddy = element du tableau ? 
+      // ouvre ta console
+      console.log(`teddy numero ${index}: `, teddy);
+
+      // on cree la div principale qui contiendra tous les sous elements
+      const divCard = document.createElement("div");
+      divCard.setAttribute("id", teddy._id);
+      divCard.classList.add("card");
+
+      // on créé la carte et ses éléments 
+      const divCardBody = document.createElement("div");
+      divCardBody.classList.add("card-body");
+      divCard.appendChild(divCardBody);
+
+      
+
+      const img = document.createElement("img");
+      img.classList.add("card-img-top");
+      img.setAttribute("src", teddy.imageUrl);
+      img.setAttribute("alt", teddy.name);
+      divCardBody.appendChild(img);
+
+      const name = document.createElement("h5");
+      name.classList.add("card-title");
+      divCardBody.appendChild(name);    
+      name.innerHTML = teddy.name;
+      
+      const price = document.createElement("p");
+      price.classList.add("card-text");
+      divCardBody.appendChild(price);
+      price.innerHTML = teddy.price;
+
+      const a = document.createElement("a");
+      a.setAttribute("href", "page-produit.html");
+      divCardBody.appendChild(a); 
+
+      const buttonCard = document.createElement("button");
+      buttonCard.classList.add("btn", "btn-primary");
+      buttonCard.setAttribute("type","button");
+      buttonCard.innerText = "Sélectionner";
+      a.appendChild(buttonCard);
+    
+      
+
+      //  on ajoute la div principale a l'element du dom selectionne plus tot (ligne 10)
+      products.appendChild(divCard);
+
+      buttonCard.onclick = function storeData(){
+      window.localStorage.setItem('productDetails', teddy);
+      console.log(localStorage.getItem('productDetails'));
+      
+        //window.open("page_produit.php", "_self");
+      };
+      
+    });
   });
-}
+};
+
+// appelle de la function afin de l'executer
+renderProducts();
+
+
+
 
 
---
+++ pageProduct.js
@@ -0,0 +1,53 @@
+// page produit : ecouter l'évenement click pour créer la page qui correspond ? 
+
+
+
+let objetProduit = window.localStorage.getItem("productDetails");
+console.log(objetProduit);
+
+
+
+
+
+
+const pageProduct = document.querySelector("#pageProduct");
+
+const product = document.createElement("div");
+      product.classList.add("row");
+      pageProduct.appendChild(product);
+
+const imgProduct = document.createElement("div");
+      imgProduct.classList.add("col-4");
+      product.appendChild(imgProduct);
+
+const listProduct = document.createElement("div");
+      listProduct.classList.add("col-6");
+      product.appendChild(listProduct);
+
+const nameProduct = document.createElement("p");
+      listProduct.appendChild(nameProduct);
+ 
+const priceProduct = document.createElement("p");
+      listProduct.appendChild(priceProduct);
+
+const descriptionProduct = document.createElement("p");
+      listProduct.appendChild(descriptionProduct);
+
+const labelProduct = document.createElement("label");
+      listProduct.appendChild(labelProduct);
+
+const selectProduct = document.createElement("select");
+      listProduct.appendChild(selectProduct);
+
+// colors.forEach(color => {
+//     const option = document.createElement('option');
+//     option.value = productColor;
+//     option.textContent = productColor;
+//     select.appendChild(option);
+// });
+
+const addToCart = document.createElement("button");
+    addToCart.innerHTML = "Ajouter au panier";
+    listProduct.appendChild(addToCart);
+
+
